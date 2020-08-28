f069e5185d5de7d2c085eb508c61ed058a72688a
37North <37northapps@gmail.com>
2020-08-29 00:35:45 +0400

    extend for users and authentication

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user_resources import UserRegister
 from resources.item_resources import Item, ItemList
 from resources.store_resources import Store, StoreList
 from db import db
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.secret_key = 'jose'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'secret_key'
 api = Api(app)
 
 
@@ -15,13 +21,13 @@ def create_tables():
     db.create_all()
 
 
-
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
-
+api.add_resource(UserRegister, '/register')
 	
 if __name__ == '__main__':
     from db import db
--- resources/item_resources.py
+++ resources/item_resources.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.item_model import ItemModel
-
+from flask_jwt import jwt_required
 
 
 
@@ -12,7 +12,7 @@ class Item(Resource):
                         help="This field cannot be left blank!"
                         )
 
-
+    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
