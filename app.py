47e9bee26c43dbc8e342d1ed8a37f20487f1f4f9
37North <37northapps@gmail.com>
2020-08-28 19:23:53 +0400

    step 2: Basic API to get, post, put, and delete items

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Api
+from resources.item_resources import Item, ItemList
+from db import db
 
-@app.route('/')
-def home():
-	return 'Hello world'
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.secret_key = 'jose'
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+
+
+api.add_resource(Item, '/item/<name>')
+
+api.add_resource(ItemList, '/items')
+
 	
-	
-app.run(port = 5151, debug = True)
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5151, debug=True)
